b15160f61034cea93c4e33cbb99e445d93fd0c2a
Rhavyx <everluca@outlook.com>
2021-09-19 09:37:27 -0300

    :art: mix colors

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main() {
 
     //Disc
@@ -14,6 +16,9 @@ void main() {
     strenght = 1.0 - strenght;
     strenght = pow(strenght, 5.0);
 
+    //Final Color
+    vec3 color = mix(vec3(0.0), vColor, strenght);
 
-    gl_FragColor = vec4(vec3(strenght), 1.0);
+
+    gl_FragColor = vec4(color, 1.0);
 }
