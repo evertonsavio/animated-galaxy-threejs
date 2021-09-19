e3ac63af798157913fcdebd31319471155c68dbd
Rhavyx <everluca@outlook.com>
2021-09-19 09:26:15 -0300

    :sparkles: applying diffuse point pattern

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,8 +1,13 @@
 void main() {
 
     //Disc
+    //float strenght = distance(gl_PointCoord, vec2(0.5));
+    //strenght = 1.0 - step(0.5, strenght);
+
+    //Diffuse Point
     float strenght = distance(gl_PointCoord, vec2(0.5));
-    strenght = 1.0 - step(0.5, strenght);
+    strenght *= 2.0;
+    strenght = 1.0 - strenght;
 
     gl_FragColor = vec4(vec3(strenght), 1.0);
 }
