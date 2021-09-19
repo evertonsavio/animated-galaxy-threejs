6d4f2e7315dc251bda608c0678f702e1673ba27e
Rhavyx <everluca@outlook.com>
2021-09-19 09:22:35 -0300

    :sparkles: applying disc pattern

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,8 @@
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+    //Disc
+    float strenght = distance(gl_PointCoord, vec2(0.5));
+    strenght = 1.0 - step(0.5, strenght);
+
+    gl_FragColor = vec4(vec3(strenght), 1.0);
 }
