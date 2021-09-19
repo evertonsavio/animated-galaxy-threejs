81aa11d48878542c33e229c6bcf5915a2e58566b
Rhavyx <everluca@outlook.com>
2021-09-19 10:01:52 -0300

    :art: animated universe portal

--- src/script.js
+++ src/script.js
@@ -101,6 +101,7 @@ const generateGalaxy = () =>
         vertexShader: vertexGalaxyShader,
         fragmentShader: fragmentGalaxyShader,
         uniforms: {
+            uTime: { value: 0 },
             uSize: { value: 10.0 * renderer.getPixelRatio()}
         }
     })
@@ -148,14 +149,17 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 1
+camera.position.z = 0
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.maxDistance = 5;
+controls.rotateSpeed = 0.05;
+controls.zoomSpeed = 0.05;
 
 /**
  * Renderer
@@ -179,6 +183,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Material
+    material.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update()
--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -3,9 +3,21 @@ uniform float uSize;
 attribute float aScale;
 
 varying vec3 vColor;
+uniform float uTime;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    modelPosition += uTime * 0.005;
+
+    //Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceOfParticleFromCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceOfParticleFromCenter) * uTime * 0.1;
+    angle += angleOffset;
+    modelPosition.x = cos(angle);
+    modelPosition.z = sin(angle);
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
